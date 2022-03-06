4cae01762d79c313f6c6131623fecc18336ae8c7
Yuichiro Kondo <kondo_u16@yahoo.co.jp>
2022-03-06 18:52:55 +0900

    Add build script

---
+++ PSDiagramGenerator/PSDiagramGenerator.psm1
@@ -0,0 +1,166 @@
+# Generated at 03/06/2022 11:18:47
+function Remove-Bracket {
+  <#
+  .SYNOPSIS
+    Remove the bracket of a CUClass like '[String]'
+  .OUTPUTS
+    String
+  #>
+  param (
+    [Parameter(Mandatory = $true)]
+    [String] $Str
+  )
+  # TODO: Make it deal with an objet array like "Object[]"
+  # return $Str.Substring(1, $Str.Length - 3)
+  return ([RegEx]::Matches($Str, "(?<=\[).*?(?=\])")).Value
+}
+function New-MermaidClassDiagram {
+  [CmdletBinding()]
+  param(
+    [Parameter(Mandatory = $true)]
+    [System.IO.FileInfo[]]$Path
+  )
+  $CUClass = Get-CUClass -Path $Path
+
+  $space = ' '
+  $tabSpace = '  '
+  $sb = '{'
+  $eb = '}'
+  $sp = '('
+  $ep = ')'
+  $comma = ','
+  $mdCode = '```'
+  $plus = '+'
+  $minus = '-'
+  $inheritance = '<|--'
+  $aggregation = 'o--'
+
+  $ClassStrArr = @()
+  $ClassStrArr += ($mdCode + 'mermaid')
+  $ClassStrArr += 'classDiagram'
+  $ClassStrArr += ''
+
+  # Generate Class relation
+  $CUClass | ForEach-Object {
+    $ClassName = $_.Name
+
+    # Inheritance
+    if ($Null -ne $_.ParentClassName) {
+      $ParentClassName = $_.ParentClassName
+    
+      $Str = $ParentClassName + $space + $inheritance + $space + $ClassName
+      $ClassStrArr += $Str
+    }
+
+    # Aggregation
+    if ($Null -ne $_.Constructor) {
+      $_.Constructor | ForEach-Object {
+        $ConstructorName = $_.Name
+        $Parameter = $_.Parameter
+
+        if ($Null -ne $Parameter) {
+          $Parameter | ForEach-Object {
+            if ('' -eq $_.Type) { return }
+            $Type = Remove-Bracket -Str $_.Type
+    
+            if ($Type -in $CUClass.Name) {
+              $Str = $ConstructorName + $space + $aggregation + $space + $Type
+              $ClassStrArr += $Str          
+            }
+          }
+        }
+      }
+    }
+  }
+
+  $ClassStrArr += ''
+
+  # Generate Class
+  $CUClass | ForEach-Object {
+
+    $ClassName = $_.Name
+    $ClassStrArr += 'class' + $space + $ClassName + $space + $sb
+
+    # Property
+    if ($Null -ne $_.Property) {
+      $_.Property | ForEach-Object {
+        $PropertyName = $_.Name
+        $Type = $_.Type
+        $Visibility = if ("Hidden" -eq $_.Visibility) { $minus } else { $plus }
+
+        $Str = $tabSpace + $Visibility + $Type + $space + $PropertyName
+        $ClassStrArr += $Str
+      }
+    }
+
+    $ClassStrArr += ''
+
+    # Constructor
+    if ($Null -ne $_.Constructor) {
+      $_.Constructor | ForEach-Object {
+        $ConstructorName = $_.Name
+        $Parameter = $_.Parameter
+
+        $Str = $tabSpace + $plus + $ConstructorName + $sp
+
+        if ($Null -ne $Parameter) {
+          $Parameter | ForEach-Object {
+            if ('' -eq $_.Type) { return }
+            $ParameterName = $_.Name
+            $Type = Remove-Bracket -Str $_.Type
+    
+            $Str = $Str + $Type + $space + $ParameterName
+            
+            # In case there are multiple parameters
+            if ($Parameter.IndexOf($_) -lt $Parameter.Count - 1) {
+              $Str = $Str + $comma + $space
+            }
+          }
+        }
+    
+        $Str = $Str + $ep + $space + $ConstructorName
+        $ClassStrArr += $Str
+      }
+    }
+
+    # Method
+    if ($Null -ne $_.Method) {
+      $_.Method | ForEach-Object {
+        $MethodName = $_.Name
+        $ReturnType = Remove-Bracket -Str $_.ReturnType
+        $Parameter = $_.Parameter
+
+        $Str = $tabSpace + $plus + $MethodName + $sp
+
+        if ($Null -ne $Parameter) {
+          $Parameter | ForEach-Object {
+            $ParameterName = $_.Name
+            $Type = Remove-Bracket -Str $_.Type
+    
+            $Str = $Str + $Type + $space + $ParameterName
+            
+            # In case there are multiple parameters
+            if ($Parameter.IndexOf($_) -lt $Parameter.Count - 1) {
+              $Str = $Str + $comma + $space
+            }
+          }
+        }
+    
+        $Str = $Str + $ep + $space + $ReturnType
+        $ClassStrArr += $Str
+      }
+    }    
+
+    # End
+    $ClassStrArr += $eb
+    $ClassStrArr += ''
+  }
+
+  $ClassStrArr += $mdCode
+
+  # $ClassStrArr | ForEach-Object {
+  #   $_
+  # }
+  Set-Clipboard -Value $ClassStrArr
+  Write-Host 'Source code of the class diagram for mermaid is copied to clipboard.'
+}
